0f4f359f2da480f08832d72aa2c4a3c4d03227bc
CBeelen <cha.beelen@gmail.com>
2021-05-13 13:26:03 -0700

    Added user-guesses mode and split computer-guesses mode into separate function

--- mastermind.py
+++ mastermind.py
@@ -1,12 +1,11 @@
 import random
 
 
-def guess_combination(possible_colors, length):
-    guess = []
+def create_random_combination(possible_colors, length):
+    random_combination = []
     for i in range(0, length):
-        guess.append(random.choice(possible_colors))
-    print(f'Guessing... {guess}')
-    return guess
+        random_combination.append(random.choice(possible_colors))
+    return random_combination
 
 
 def evaluate_guess(combination, guess):
@@ -33,9 +32,8 @@ def evaluate_guess(combination, guess):
     return num_white, num_black
 
 
-def main():
-    print("Welcome to Mastermind, please choose the colors of your pegs. The colors are: red, orange, yellow, green, blue, white, brown, black")
-    possible_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'white', 'brown', 'black']
+def input_combination(possible_colors):
+    print(f"Please input a combination. The colors are: {possible_colors}")
     combination = []
     txt = "Color of peg {} "
     for i in range(1,6):
@@ -44,9 +42,35 @@ def main():
             message = "{} is not a possible color. Please choose again: "
             color_new = input(message.format(color_new))
         combination.append(color_new)
+    return combination
+
+
+def computer_guesses(possible_colors):
+    print("I will try to guess your combination.")
+    combination = input_combination(possible_colors)
     print("Guessing combination...")
-    guess = guess_combination(possible_colors, len(combination))
+    guess = create_random_combination(possible_colors, len(combination))
+    print(f'Guessing... {guess}')
     num_white, num_black = evaluate_guess(combination, guess)
+    if num_white == 5:
+        print("I have guessed the combination!")
+
+
+def user_guesses(possible_colors):
+    combination = create_random_combination(possible_colors, 5)
+    print("Try to guess my combination!")
+    guess = input_combination(possible_colors)
+    num_white, num_black = evaluate_guess(combination, guess)
+    while num_white != 5:
+        print("Try again!")
+        guess = input_combination(possible_colors)
+        num_white, num_black = evaluate_guess(combination, guess)
+    print("You've guessed it!!")
+
+def main():
+    possible_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'white', 'brown', 'black']
+
+    user_guesses(possible_colors)
 
 
 main()
